5a82fe5ff1517e93646069dbe029bb45f3e58438
MasakiFukunishi <fmsknn@gmail.com>
2021-07-04 23:54:26 +0900

    adde candle chart

--- src/app/controllers/webserver.py
+++ src/app/controllers/webserver.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask import jsonify
 from flask import render_template
+from flask import request
 import sys
 sys.path.append('/src')
+
+from app.models.dfcandle import DataFrameCandle
+
+import constants
 import settings
 
 app = Flask(__name__, template_folder='../views')
@@ -15,9 +21,29 @@ def remove_session(ex=None):
 
 @app.route('/')
 def index():
-    app.logger.info('index')
-    return render_template('./google.html',
-                           word='Worlaad')
+    return render_template('./chart.html')
+
+@app.route('/api/candle/', methods=['GET'])
+def api_make_handler():
+    product_code = request.args.get('product_code')
+    if not product_code:
+        return jsonify({'error': 'No product_code params'}), 400
+
+    limit_str = request.args.get('limit')
+    limit = 1000
+    if limit_str:
+        limit = int(limit_str)
+
+    if limit < 0 or limit > 1000:
+        limit = 1000
+
+    duration = request.args.get('duration')
+    if not duration:
+        duration = constants.DURATION_1M
+    duration_time = constants.TRADE_MAP[duration]['duration']
+    df = DataFrameCandle(product_code, duration_time)
+    df.set_all_candles(limit)
+    return jsonify(df.value), 200
 
 # if __name__ == '__main__':
 #     app.run(host='0.0.0.0', port=8888, debug=True)
