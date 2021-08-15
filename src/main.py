9d96dc49da7c270f862fda94ae80db4939d0ecba
MasakiFukunishi <fmsknn@gmail.com>
2021-08-15 21:55:40 +0900

    changed duratiom

--- src/main.py
+++ src/main.py
@@ -17,12 +17,36 @@ if __name__ == "__main__":
 #     data = DataProcess()
 #     data.delete_candles_recursive()
 
-    data = DataProcess()
-    dataThread = Thread(target=data.delete_candles_recursive)
     streamThread = Thread(target=stream.stream_ingestion_data)
-    
-    dataThread.start()
     streamThread.start()
-    
-    dataThread.join()
     streamThread.join()
+    
+
+# import logging
+# import sys
+# from threading import Thread
+
+# from app.controllers.streamdata import stream
+# from app.controllers.initialprocess import InitialProcess
+# from app.controllers.dataprocess import DataProcess
+
+# logging.basicConfig(filename="console.log",level=logging.INFO)
+# # logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+
+
+# if __name__ == "__main__":
+#     initial = InitialProcess()
+#     initial.set_initial_candles()
+    
+# #     data = DataProcess()
+# #     data.delete_candles_recursive()
+
+#     data = DataProcess()
+#     dataThread = Thread(target=data.delete_candles_recursive)
+#     streamThread = Thread(target=stream.stream_ingestion_data)
+    
+#     dataThread.start()
+#     streamThread.start()
+    
+#     dataThread.join()
+#     streamThread.join()
